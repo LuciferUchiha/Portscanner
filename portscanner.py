1c5055489c44c65f9c0ac3abf50c765f06d9f18d
Lucifer Uchiha <rowla101@gmail.com>
2021-09-15 13:04:59 +0200

    Cleande up code and added Progress bar

--- portscanner.py
+++ portscanner.py
@@ -3,23 +3,19 @@ import sys
 import argparse
 import concurrent.futures
 import pyfiglet
+from tqdm import tqdm
+
+ports = []
+host_IP = ""
 
 
-def scan_tcp(host_IP, port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket.setdefaulttimeout(1)
-    try:
-        result = sock.connect_ex((host_IP, port))  # returns “0” if connection goes
-        if result == 0:
-            print(f"Port {port} is OPEN")
-        sock.close()
-
-    except socket.error:
-        print(f"Could not connect to host '{host_IP}'")
-        sys.exit()
-    except KeyboardInterrupt:
-        print("Exiting...")
-        sys.exit()
+def resolve_args():
+    argv = sys.argv[1:]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("host", type=host, help="Hostname or IP of host system to be scanned")
+    parser.add_argument("-s", "--startPort", type=port, default=0, help="Port number to start scan (0-65535)")
+    parser.add_argument("-e", "--endPort", type=port, default=65535, help="Port number to end scan (0-65535)")
+    return vars(parser.parse_args(argv))
 
 
 def host(s):
@@ -40,24 +36,40 @@ def port(s):
     return port
 
 
-def resolve_args():
-    argv = sys.argv[1:]
-    parser = argparse.ArgumentParser()
-    parser.add_argument("host", type=host, help="Hostname or IP of host system to be scanned")
-    parser.add_argument("-s", "--startPort", type=port, default=0, help="Port number to start scan (0-65535)")
-    parser.add_argument("-e", "--endPort", type=port, default=65535, help="Port number to end scan (0-65535)")
-    return vars(parser.parse_args(argv))
+def scan_tcp(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(1)
+    try:
+        result = sock.connect_ex((host_IP, port))
+        if result == 0:
+            # print(f"Port {port} is OPEN")
+            return port
+        sock.close()
+
+    except socket.error:
+        print(f"Could not connect to host '{host_IP}'")
+        sys.exit()
+    except KeyboardInterrupt:
+        print("Exiting...")
+        sys.exit()
+
+
+def print_open_ports(results):
+    results = list(filter(None, results))
+    for open_port in results:
+        print(f"Port {open_port} is OPEN")
 
 
 def main():
     args = resolve_args()
     pyfiglet.print_figlet("PORTSCANNER", font="slant")
     host_IP = args.get("host")
+    ports = range(args.get("startPort"), args.get("endPort") + 1)
     print(f"Starting scan of {host_IP}...")
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        for port in range(args.get("startPort"), args.get("endPort") + 1):
-            executor.submit(scan_tcp, host_IP, port)
+        results = list(tqdm(executor.map(scan_tcp, ports), total=len(ports)))
     print(f"Finished scan of {host_IP}!")
+    print_open_ports(results)
 
 
 if __name__ == "__main__":
